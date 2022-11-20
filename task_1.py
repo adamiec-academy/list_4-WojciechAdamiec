05328721f691c0d9e56b4ed560e6c2048031af52
Wojciech Adamiec <56234206+WojciechAdamiec@users.noreply.github.com>
2022-11-20 12:46:02 +0100

    Update task_1.py

--- task_1.py
+++ task_1.py
@@ -1,2 +1,4 @@
 def is_palindrome(text):
-    return False
+    transformed_text = text.lower()
+    transformed_text = ''.join(transformed_text.split())
+    return transformed_text == transformed_text[::-1]
