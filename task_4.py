d4893af25e65edae0c4aeda0b0f786821e306d5b
Wojciech Adamiec <56234206+WojciechAdamiec@users.noreply.github.com>
2022-11-20 12:29:19 +0100

    Update task_4.py

--- task_4.py
+++ task_4.py
@@ -6,4 +6,15 @@ def info(data):
 
 
 def border_map(a, b):
-    return [['.']]
+    matrix = [["." for _ in range(a)] for _ in range(b)]
+    x_size, y_size = len(matrix[0]), len(matrix)
+
+    for y in range(y_size):
+        matrix[y][0] = "X"
+        matrix[y][x_size - 1] = "X"
+
+    for x in range(x_size):
+        matrix[0][x] = "X"
+        matrix[y_size - 1][x] = "X"
+
+    return matrix
